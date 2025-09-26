527fc4bf30fa4e866362320c1dc41ec7658aa0c9
s-orai <rai1125.30706@gmail.com>
2025-09-26 13:35:12 +0900

    抽出項目追加

--- logic.py
+++ logic.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import json
 import streamlit as st
+from definitions import incentive, actual_bonus_payments, prefectures_reverse, num_of_bonuses, workstyle, relocation, positions, commission_earned_at, night_time_shift, overtime, job_categories
 
 # APIのエンドポイント
 api_login_url = st.secrets["api_url"]["session"]
@@ -45,7 +46,7 @@ def job_search(token, keyword, keyword_category, keyword_option, min_salary, max
 
     qjson = {"option": keyword_category, "keyword": keyword, "logicType": keyword_option}
 
-    all_jobs = []
+    jobs = []
     offset = 0
     limit = 25  # 1ページあたりの取得件数
 
@@ -75,17 +76,29 @@ def job_search(token, keyword, keyword_category, keyword_option, min_salary, max
         # ステータスコード確認
         print("Status Code:", response.status_code)
 
-        # JSONデータを取得
-        if response.status_code == 200 or response.status_code == 201:
-            data = response.json()["jobs"]
-            if not data:
-                break  # データがもうない場合はループを終了
-            all_jobs.extend(data)
-            offset += limit
-        else:
+        if response.status_code != 200 and response.status_code != 201:
             print("求人取得に失敗しました。Error:", response.text)
             exit(1) # Stop execution on job search failure
-    return all_jobs
+
+        # JSONデータを取得
+        data = response.json()["jobs"]
+        if not data:
+            break  # データがもうない場合はループを終了
+        jobs.extend(data)
+        offset += limit
+
+    job_details = []
+    # リクエスト送信(詳細情報など)
+    for job in jobs:
+        job_id = job["id"]
+        response = requests.get(api_job_serach_url, headers=headers, params=[("id", job_id)])
+
+        if response.status_code != 200 and response.status_code != 201:
+          print(f"求人取得に失敗しました。求人ID: {job_id}, Error:{response.text}")
+          continue
+        job_details.append(response.json())
+
+    return job_details
 
 # 再帰的にフラット化する関数
 def flatten_json(y, prefix=''):
@@ -111,8 +124,28 @@ def create_job_df(json_data):
     flat_data = [flatten_json(d) for d in json_data]
     df = pd.DataFrame(flat_data)
 
+    df["職種"] = df["occupations.main"].map(job_categories)
+    df["想定年収"] = df.apply(lambda row: f"{row["expectedAnnualSalary.min"]}万円~{row["expectedAnnualSalary.max"]}万円", axis=1)
+    df["月給"] = df.apply(lambda row: f"{row["expectedMonthlySalary.min"]}万円~{row["expectedMonthlySalary.max"]}万円", axis=1)
+
+    df["勤務地"] = df.apply(lambda row: collect_values(row, "addresses", prefectures_reverse, "prefecture"), axis=1)
+    df["職位"] = df.apply(lambda row: collect_values(row, "positions", positions), axis=1)
+
+    df["賞与回数"] = df["frequencyOfBonusPayments"].map(num_of_bonuses)
+    df["昨年度賞与実績"] = df["actualBonusPaymentsLastYear"].map(actual_bonus_payments)
+    df["インセンティブ"] = df["incentive"].map(incentive)
+
+    df["勤務形態"] = df.apply(lambda row: collect_values(row, "workstyles", workstyle), axis=1)
+
+    df["転勤の可能性"] = df["relocationProbability"].map(relocation)
+    df["勤務時間"] = df.apply(lambda row: f"{row["workHours.start"]}~{row["workHours.end"]}", axis=1)
+    df["夜間勤務"] = df["nightTimeShift"].map(night_time_shift)
+    df["月刊平均残業時間"] = df["averageOvertime"].map(overtime)
+    df['成果報酬金額'] = df.apply(format_commission, axis=1)
+    df["成果地点"] = df["commissionEarnedAt"].map(commission_earned_at)
+
     # 抽出項目を絞り込み
-    df = df[["id", "name", "company.name", "minimumQualification", "occupations.main", "addresses.0.prefecture", "expectedAnnualSalary.min", "expectedAnnualSalary.max", "jobDescriptions"]]
+    df = df[["id", "name", "company.name", "職種", "職位", "想定年収", "月給", "勤務地", "minimumQualification", "jobDescriptions", "賞与回数", "昨年度賞与実績", "インセンティブ", "annualSalaryExample", "salaryComments", "addressDetail", "勤務形態", "転勤の可能性", "勤務時間", "夜間勤務", "月刊平均残業時間", "locationComments", "成果報酬金額", "成果地点"]]
 
     # 項目名を変更
     df = df.rename(columns={
@@ -120,11 +153,11 @@ def create_job_df(json_data):
         "name": "求人名",
         "company.name": "募集企業名",
         "minimumQualification": "応募必須条件",
-        "occupations.main": "職種",
-        "addresses.0.prefecture": "勤務地",
-        "expectedAnnualSalary.min": "年収下限",
-        "expectedAnnualSalary.max": "年収上限",
-        "jobDescriptions": "仕事内容"
+        "jobDescriptions": "仕事内容",
+        "annualSalaryExample": "年収例",
+        "salaryComments": "給与・年収例 補足情報",
+        "addressDetail": "勤務地詳細",
+        "locationComments": "勤務地・勤務時間 補足情報",
     })
 
     return df
@@ -167,3 +200,39 @@ def job_count(token, keyword, keyword_category, keyword_option, min_salary, max_
     else:
         print("求人取得に失敗しました。Error:", response.text)
         exit(1) # Stop execution on job search failure
+
+
+def collect_values(row, prefix: str, mapping: dict, suffix: str = "", sep: str = "、"):
+    """
+    row: df.apply で渡される1行
+    prefix: 列名のプレフィックス (例: 'addresses.')
+    suffix: 列名のサフィックス (例: '.prefecture')
+    mapping: 対応辞書 (例: prefectures_reverse)
+    sep: 結合時の区切り文字
+    """
+    results = []
+    i = 0
+    while True:
+        parts = [prefix, str(i)]
+        if suffix:
+            parts.append(suffix)
+        col = ".".join(parts)
+
+        if col not in row:
+            break
+        val = row[col]
+        if pd.notna(val):
+            results.append(mapping.get(val, "不明"))
+        i += 1
+    return sep.join(results)
+
+def format_commission(row):
+    try:
+        fee = row['commissionFee.fee']
+        cid = row['commissionFee.id']
+    except KeyError:
+        return None
+    if cid == 1:
+        return f"理論年収×{fee}%"
+    else:
+        return f"{fee:,}円"
